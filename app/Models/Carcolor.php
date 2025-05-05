de9d2369370f6f7824a2452bbf0352b51eb5f907
CongNhut1210 <nhuttran12102000@gmail.com>
2025-05-05 18:18:52 +0700

    fix CarColor , detele  migration cas

--- app/Models/Carcolor.php
+++ app/Models/Carcolor.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Carcolor extends Model
+class CarColor extends Model
 {
     use HasFactory;
 
