c409b02123116e16cf58dd95c3b942dab067784e
Thanh Toan <thanhtoan.nguyenviet@gmail.com>
2025-05-08 22:50:37 +0700

    Fix conflict

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\BuyController;
 
@@ -9,3 +11,10 @@ Route::get('/cars/{id}', [PageController::class, 'carDetail']);
 Route::get('/buy/{id?}', [PageController::class, 'buyForm']);
 Route::post('/buy/submit', [BuyController::class, 'submit']);
 Route::get('/contact', [PageController::class, 'contact']);
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::resource('users', UserController::class)
+    ->middleware('auth');
+
